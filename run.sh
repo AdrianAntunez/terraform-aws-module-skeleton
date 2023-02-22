27b561b9f992da650dd0dcff51fbab70cd1a88ec
AdrianAntunez <adriananve@gmail.com>
2023-02-22 11:53:32 +0100

    Small fixes

--- run.sh
+++ run.sh
@@ -9,18 +9,19 @@ cd $script_dir
 # Use docker-compose to run terraform commands
 if [ $# -ge 1 ]; then
     if [[ "$1" == "fmt" ]]; then
-        $DOCKER_COMPOSE_COMMAND terraform -chdir=./test fmt --recursive ${@:2} ../ 
+        $DOCKER_COMPOSE_COMMAND terraform fmt --recursive ${@:2}
     else
         $DOCKER_COMPOSE_COMMAND terraform -chdir=./test ${@:1}
     fi
 else
     echo -e """
     Help: 
-        $0 ftm [extra_terraform_parameters]
-            Runs terraform format recursivelly in all module directory and its subdirectories
-            [extra_terraform_parameters]: Any extra terraform parameters for fmt e.g: -list=false
+        $0 fmt [extra_terraform_parameters]
+            Runs terraform format on root folder recursivelly
+            [extra_terraform_parameters]: Any extra terraform parameters for fmt e.g: -list=false...
 
         $0 terraform_command [extra_terraform_parameters]
+            Runs any terraform command (except terraform fmt) in test/ folder
             terraform_command: Any terraform command, e.g: init, validate...
             [extra_terraform_parameters]: Any extra terraform parameters, e.g: --reconfigure
     """
